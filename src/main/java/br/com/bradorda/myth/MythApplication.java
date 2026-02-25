3e7cd77e3ddceb5b389c1f447e7d26d168130c7c
Leonardo Bradorda <Theleonardovictorteodoro@gmail.com>
2026-02-25 19:59:55 -0300

    startando o repositorio

---
+++ src/main/java/br/com/bradorda/myth/MythApplication.java
@@ -0,0 +1,13 @@
+package br.com.bradorda.myth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MythApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MythApplication.class, args);
+	}
+
+}

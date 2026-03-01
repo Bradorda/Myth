2679c911055ddc4fad565d1536dc2b79b1cfbeaa
Leonardo Bradorda <Theleonardovictorteodoro@gmail.com>
2026-03-01 20:28:29 -0300

    loadUserByUsername -> vinculando pessoa ao User.

--- src/main/java/br/com/bradorda/myth/configs/security/UserAuthConfig.java
+++ src/main/java/br/com/bradorda/myth/configs/security/UserAuthConfig.java
@@ -1,5 +1,7 @@
 package br.com.bradorda.myth.configs.security;
 
+import br.com.bradorda.myth.entities.User;
+import br.com.bradorda.myth.repositories.PessoaRepository;
 import br.com.bradorda.myth.repositories.UserRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -10,16 +12,22 @@ import org.springframework.stereotype.Service;
 public class UserAuthConfig implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final PessoaRepository pessoaRepository;
 
-    public UserAuthConfig(UserRepository userRepository) {
+    public UserAuthConfig(UserRepository userRepository, PessoaRepository pessoaRepository) {
         this.userRepository = userRepository;
+        this.pessoaRepository = pessoaRepository;
     }
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
 
-        return userRepository.findByPessoaEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException(String.format("E-mail %s não foi encontrado ", username)));
+        User user = userRepository.findByPessoaEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException(
+                        String.format("E-mail %s não foi encontrado ", username)));
 
+        user.setPessoa(pessoaRepository.findByEmail(username));
+
+        return user;
     }
 }
